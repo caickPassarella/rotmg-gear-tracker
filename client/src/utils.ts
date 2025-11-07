9940b0773b0e0f4987c141654e7928e877458d8c
Caick <caickpassarella@gmail.com>
2025-11-06 22:17:26 -0300

    Adds env variable

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,6 +1,6 @@
 import type { ClassName } from "./types";
 
 export const fetchRotmgGear = async (className: ClassName) => {
-  const data = await fetch(`http://localhost:3000/top/${className}`);
+  const data = await fetch(`${import.meta.env.VITE_API_URL}top/${className}`);
   return data.json();
 };
